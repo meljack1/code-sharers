300703ae15fe0a8a944cb4d3207df7e802dc6f3a
Cleggatron <david.robert.clegg@gmail.com>
2021-11-12 16:57:37 +0000

    added basics of typedefs and resolvers.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+const { AuthenticationError } = require("apollo-server-express");
+// ToDo import models here
+
+// ToDo Import utility functions here
+
+const resolvers = {}
+
+module.exports = resolvers;
