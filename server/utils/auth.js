e094684feed4282864439d07c6e62676d453cd69
Mustafa Ali <mustafamire@gmail.com>
2021-11-16 19:20:09 +0000

    allow tokens  to be sent  via req.body/req.query/headers

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,4 +5,11 @@ const secret = "codesharerssecret";
 const expiration = "2h"; 
 
 
+module.exports = {
+    authMiddleware: function ({ req }) {
+      // allows token to be sent via req.body, req.query, or headers
 
+      let token = req.body.token || req.query.token || req.headers.authorization;
+    }
+    
+}
