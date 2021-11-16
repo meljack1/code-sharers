d0e5f1dad0c6b229f65e2c2fcbd09ca5054a3d88
Mustafa Ali <mustafamire@gmail.com>
2021-11-16 19:23:05 +0000

    Add logic to split + pop + trim token string and return an actual array

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,6 +10,11 @@ module.exports = {
       // allows token to be sent via req.body, req.query, or headers
 
       let token = req.body.token || req.query.token || req.headers.authorization;
+
+      // We split the token string into an array and return actual token
+    if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+      }
     }
     
 }
