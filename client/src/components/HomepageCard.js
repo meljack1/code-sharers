b765b6f85cede5fc67c30dbaf5e4ca56f0ad7183
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-18 21:36:29 +0000

    Add props to homepage card

--- client/src/components/HomepageCard.js
+++ client/src/components/HomepageCard.js
@@ -6,19 +6,21 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 
-export default function HomepageCard() {
+export default function HomepageCard(props) {
   return (
     <Card sx={{margin: 2}}>
       <CardContent>
-        <Typography color="text.secondary" variant="h5">
-            {/* TO DO: Import props for title, language, and description */}
-          Title - Language
+        <Typography color="text.secondary" variant="h5" gutterBottom>
+          {props.name} - {props.language}
         </Typography>
-        <Typography variant="body2">
-          Lorem ipsum ldfdjflkdj fkjdkfdf kjfkelfjekfdkf dfjdkmmk mkcvk
+        <Typography variant="body2" gutterBottom>
+          {props.description}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary">
+          Posted on {props.date}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions sx={{background: "lightGrey"}}>
         <Button size="small" sx={{margin: "auto"}}>See full code</Button>
       </CardActions>
     </Card>
