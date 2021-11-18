6476dda8abfdaa4b83a9c0b0ab2dc236f3b14d0e
Cleggatron <david.robert.clegg@gmail.com>
2021-11-18 21:13:38 +0000

    added getme query

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import { useMutation, useQuery} from "@apollo/client";
 
+import {GET_ME} from "./utils/queries"
+
 import SnippetForm from "./SnippetForm"
 
 
@@ -8,8 +10,9 @@ import Auth from "./utils/auth";
 
 const Dashboard = () => {
     //add in me query here
-    const {loading, data} = useQuery()
+    const {loading, data} = useQuery(GET_ME)
     const userData = data?.me || {}
+
     if (loading){
         return <h1>Please wait while the page loads.</h1>
     }
