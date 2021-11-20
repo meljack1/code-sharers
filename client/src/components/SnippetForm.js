ca56f993e7c9e1eda1bfab7e01fde330abaf90a1
Cleggatron <david.robert.clegg@gmail.com>
2021-11-20 10:48:21 +0000

    added base snippet form functionality

---
+++ client/src/components/SnippetForm.js
@@ -0,0 +1,126 @@
+import React, {useState} from "react";
+import { Box, TextField, Button} from "@mui/material"
+import { useMutation } from "@apollo/client"
+
+import { SAVE_SNIPPET } from "../utils/mutations"
+
+
+
+function SnippetForm(){
+    //Set our state
+    const [name, setName] =  useState("");
+    const [description, setDescription] =  useState("");
+    const [language, setLanguage] =  useState("");
+    const [code, setCode] = useState("");
+
+    //Use mutation
+    const [saveSnippet, {error, data}] = useMutation(SAVE_SNIPPET);
+
+    const handleInputChange = (event) => {
+        const {target} = event;
+        const inputType = target.id;
+        const inputValue = target.value;
+
+        switch(inputType){
+            case "name":
+                setName(inputValue);
+                break;
+            case "description":
+                setDescription(inputValue);
+                break;
+            case "language":
+                setLanguage(inputValue);
+                break;
+            case "code":
+                setCode(inputValue);
+                break;
+            default:
+                console.log("Invalid Target")
+                return;
+        }
+    }
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        //handle invalid submission
+        if(!name || !description || !language || !code){
+
+        }
+
+        const submission = {
+            name,
+            description,
+            language,
+            code
+        }
+
+        try {
+            const {data} = await saveSnippet({
+                variables: {input: submission}
+            })
+        } catch (err){
+            console.log(err)
+        }
+    }
+
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleFormSubmit}
+        >
+            <div>
+                <TextField
+                required
+                id="name"
+                label="Name"
+                placeholder="Name"
+                value={name}
+                onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <TextField
+                required
+                id="description"
+                label="Description"
+                placeholder="Enter a description of your code here!"
+                multiline
+                rows={5}
+                value={description}
+                onChange={handleInputChange}/>
+            </div>
+            <div>
+                <TextField
+                required
+                id="language"
+                label="Language"
+                placeholder="Enter the coding language"
+                value={language}
+                onChange={handleInputChange}/>
+            </div>
+            <div>
+                <TextField
+                required
+                id="code"
+                label="Code"
+                placeholder="Enter your code here!"
+                multiline
+                rows={5}
+                value={code}
+                onChange={handleInputChange}/>
+            </div>
+            <Button 
+                type="submit">
+                Save Snippet
+            </Button>
+        </Box>
+    )
+}
+
+export default SnippetForm
