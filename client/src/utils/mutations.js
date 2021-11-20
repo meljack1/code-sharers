d80e36c9fa77df83ee47556589565ed0e6ceb391
Cleggatron <david.robert.clegg@gmail.com>
2021-11-20 10:48:55 +0000

    added save snipped mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client"
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!"){
+    mutation addUser($username: String!, $email: String!, $password: String!){
         addUser(username: $username, email: $email, password: $password){
             token
             user{
@@ -12,12 +12,28 @@ export const ADD_USER = gql`
     }`
 
 export const LOGIN_USER = gql`
-    mutation login($email: String!, $passsword: $String!){
+    mutation login($email: String!, $passsword: String!){
         login(email: $email, password: $password){
             token
             user {
                 _id
                 username
+            }
+        }
+    }`
+
+export const SAVE_SNIPPET = gql`
+    mutation saveSnippet($input: SnippetInput!){
+        saveSnippet(input: $input){
+            _id
+            username
+            email
+            codeSnippets{
+                name
+                description
+                language
+                code
+                createdOn
             }
         }
     }`
