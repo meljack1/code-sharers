68b29fbf67e3f4637c36c2ab9f901c75f6493400
Cleggatron <david.robert.clegg@gmail.com>
2021-11-21 11:58:45 +0000

    fixed query so wouldn't give error

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,21 @@
 import { gql } from "@apollo/client"
 
 export const GET_ME = gql`
-    query{
-        me {
+query{
+    me {
+        _id
+        username
+        email
+        codeSnippets{
             _id
-            username
-            email
-            codeSnippets{
-                _id
-                name
-                description
-                language
-                code
-                createdOn
-                userId
-            }
+            name
+            description
+            language
+            code
+            createdOn
         }
-    }`
+    }
+}`
 
 export const SNIPPETS = gql`
     query{
