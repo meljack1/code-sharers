c3898c1f006309f6736c542d625704e2d7dcc6b0
Cleggatron <david.robert.clegg@gmail.com>
2021-11-21 11:14:29 +0000

    updated details and removed unecessary dependency

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { useMutation, useQuery} from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Card, Typography, CardContent} from '@mui/material'
 
 
@@ -41,6 +41,12 @@ const Dashboard = () => {
                           <Typography sx={{ fontSize: 14 }} color="text.secondary">
                             Posted on {snippet.date}
                           </Typography>
+                          <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                            Created By {snippet.userId.username}
+                          </Typography>
+                          <Link href={`/me/${snippet._id}`}>
+                            <Button size="small" sx={{margin: "auto"}}>Edit</Button>
+                          </Link>
                         </CardContent>
                       </Card>
                     )
