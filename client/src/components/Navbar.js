11c576e37e3b49e03f713a2d2a7382f247c42e3c
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-22 10:52:09 +0000

    Make navbar flexibly render links at higher screen sizes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,42 +1,97 @@
 import React from 'react';
-import Link from '@mui/material/Link';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import {
+  Link, 
+  AppBar, 
+  Box, 
+  Toolbar, 
+  Typography, 
+  Button, 
+  IconButton,
+  Menu,
+  MenuItem, 
+  useTheme, 
+  useMediaQuery
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
-
 export default function ButtonAppBar() {
-    return (
+    // Import theme to use breakpoints
+    const theme = useTheme();
 
+    // Media query - anything larger than small is a large screen
+    const largeScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
+    // Handles appearance and closing of small screen menu bar
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    return (
+      <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
+            <Typography variant="h6" component="div" sx={{ fontSize: "3ch", flexGrow: 1 }}>
+              codeShare.
+            </Typography>
+            {largeScreen ? (
+              <Box>
+                <Link href="/">
+                  <Button sx={{color: "white"}} color="inherit">Home</Button>
+                </Link>
+                <Link href="/me">
+                  <Button sx={{color: "white"}} color="inherit">My Page</Button>
+                </Link>
+                <Link href="/login">
+                  <Button sx={{color: "white"}} color="inherit">Login</Button>
+                </Link>
+              </Box>
+            ) : ( 
+              <IconButton
               size="large"
               edge="start"
               color="inherit"
               aria-label="menu"
-              sx={{ mr: 2 }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              codeShare.
-            </Typography>
-            <Link href="/">
-              <Button sx={{color: "white"}} color="inherit">Main Page</Button>
-            </Link>
-            <Link href="/me">
-              <Button sx={{color: "white"}} color="inherit">My Page</Button>
-            </Link>
-            <Link href="/login">
-              <Button sx={{color: "white"}} color="inherit">Login</Button>
-            </Link>
+              <MenuIcon 
+                aria-controls="basic-menu"
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              />
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  'aria-labelledby': 'basic-button',
+                }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Link href="/" underline="none" color="inherit">
+                    Home
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <Link href="/me" underline="none" color="inherit">
+                    My Page
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <Link href="/login" underline="none" color="inherit">
+                    Login
+                  </Link>
+                </MenuItem>
+              </Menu>
+              </IconButton>
+             )}
           </Toolbar>
         </AppBar>
-
+      </Box>
     );
   }
