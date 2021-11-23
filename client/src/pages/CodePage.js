1639f1ae1c0818c39bf511620df55ac52fd55b45
Cleggatron <david.robert.clegg@gmail.com>
2021-11-23 21:08:20 +0000

    added comment viewing

--- client/src/pages/CodePage.js
+++ client/src/pages/CodePage.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom"
 import { useQuery } from '@apollo/client';
 
 import { SNIPPET_BY_ID } from '../utils/queries';
-import {Typography, Box, Container} from '@mui/material/';
+import {Typography, Box, Container, Card, CardContent} from '@mui/material/';
 
 import { formatDate } from "../utils/date"
 
@@ -17,7 +17,7 @@ export default function CodePage() {
     },
     fetchPolicy: "no-cache",
   });
-
+  
   return (
     loading ? (<p>Loading</p>) : 
     <Container sx={{ borderLeft: 1, borderRight: 1, pt: 3, backgroundColor: "white", minHeight: "calc(100vh - 64px)" }}>
@@ -48,6 +48,27 @@ export default function CodePage() {
           <Typography sx={{ fontSize: "2.5ch" }} color="text.secondary">
             Created By: {data.snippetById.userId.username}
           </Typography>
+
+        </Box>
+
+        <Box>
+        {data.snippetById.comments.map((comment)=> {
+            return(
+            <Card variant={"outlined"}>
+              <CardContent>
+                <Typography sx={{ fontSize: "2.5ch" }} color="text.secondary">
+                  User {comment.commentAuthor} posted: 
+                </Typography>
+                <Typography sx={{ fontSize: "2.5ch" }} color="text.secondary">
+                  {comment.commentText} 
+                </Typography>
+                <Typography sx={{ fontSize: "2.5ch" }} color="text.secondary">
+                  At: {formatDate(comment.commentDate)} 
+                </Typography>
+              </CardContent>
+            </Card>
+            )
+          })}
         </Box>
       </Box>
     </Container>
