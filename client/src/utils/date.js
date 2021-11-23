aae603db6fc8c09299da9fa45986fafabf3e4082
Cleggatron <david.robert.clegg@gmail.com>
2021-11-23 18:18:14 +0000

    added date format function

---
+++ client/src/utils/date.js
@@ -0,0 +1,9 @@
+export const formatDate = (d) => {
+  let date = new Date(parseInt(d))
+  let dd = date.getDate(); 
+  let mm = date.getMonth()+1;
+  let yyyy = date.getFullYear(); 
+  if(dd<10){dd='0'+dd} 
+  if(mm<10){mm='0'+mm};
+  return d = dd+'/'+mm+'/'+yyyy
+}
